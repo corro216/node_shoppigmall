9b1e5ab7b64b2686e257228ccc9985f90228262c
sunghyun <corro216@o.cnu.ac.kr>
2020-07-31 12:21:02 +0900

    my first node webserver

---
+++ server.js
@@ -0,0 +1,19 @@
+
+
+const express = require('express'); //express 상수화
+const app = express();
+ 
+
+
+
+
+const port = 7000;
+
+
+
+app.listen(port, console.log('server started'));  //port통해서 서버를 실행할거고 log를 찍어준다.
+
+
+
+
+
