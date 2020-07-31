942a83600ab2a29639dc097f1cdd9d1037514536
sunghyun <corro216@o.cnu.ac.kr>
2020-07-31 12:44:22 +0900

    res req test

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ const app = express();
  
 
 
+// request 와 response 의 테스팅
+app.use((req, res) => {
+    res.json({
+        message : 'fighting'
+    });
+});
+
+
+
 
 
 const port = 7000;
-
-
 
 app.listen(port, console.log('server started'));  //port통해서 서버를 실행할거고 log를 찍어준다.
 
